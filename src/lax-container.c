cbfd37b99bbcb2e331efb3833e7f7ba33ae7b52a
chouettevan <pybots743@gmail.com>
2025-11-01 10:37:54 -0400

    do not log brk syscall in strict conteiner

--- src/lax-container.c
+++ src/lax-container.c
@@ -1,27 +1,30 @@
-#include <stdio.h>
-#include <sys/mman.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <linux/seccomp.h>
-#include <seccomp.h>
 #include <linux/filter.h>
+#include <linux/seccomp.h>
+#include <sched.h>
+#include <seccomp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/mount.h>
 #include <sys/ptrace.h>
 #include <sys/syscall.h>
-#include <sys/mount.h>
-#include <sched.h>
-#include <sys/syscall.h>
+#include <unistd.h>
 
-int main(int argc,char *argv[]) {
-	if (argc < 2) {
-		puts("usage: ./isolate bin");
-		return 1;
-	}
-	scmp_filter_ctx context = seccomp_init(SCMP_ACT_LOG);	
-	seccomp_rule_add(context,SCMP_ACT_KILL_PROCESS,SCMP_SYS(mount),0);
-	seccomp_rule_add(context,SCMP_ACT_ALLOW,SCMP_SYS(write),1,SCMP_A0(SCMP_CMP_EQ,1));
-	seccomp_rule_add(context,SCMP_ACT_ALLOW,SCMP_SYS(mmap),0);
-	seccomp_rule_add(context,SCMP_ACT_LOG,SCMP_SYS(mmap),1,SCMP_A2(SCMP_CMP_MASKED_EQ,PROT_EXEC,1));
-	seccomp_load(context);
-	execl(argv[1],argv[1],NULL);
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    puts("usage: ./isolate bin");
+    return 1;
+  }
+  scmp_filter_ctx context = seccomp_init(SCMP_ACT_LOG);
+  seccomp_rule_add(context, SCMP_ACT_KILL_PROCESS, SCMP_SYS(mount), 0);
+  seccomp_rule_add(context, SCMP_ACT_KILL_PROCESS, SCMP_SYS(ptrace), 0);
+  seccomp_rule_add(context, SCMP_ACT_ALLOW, SCMP_SYS(write), 1,
+                   SCMP_A0(SCMP_CMP_EQ, 1));
+  seccomp_rule_add(context, SCMP_ACT_ALLOW, SCMP_SYS(mmap), 0);
+  seccomp_rule_add(context, SCMP_ACT_ALLOW, SCMP_SYS(brk), 0);
+  seccomp_rule_add(context, SCMP_ACT_LOG, SCMP_SYS(mmap), 1,
+                   SCMP_A2(SCMP_CMP_MASKED_EQ, PROT_EXEC, 1));
+  seccomp_load(context);
+  execl(argv[1], argv[1], NULL);
 }
--- src/strict-container.c
+++ src/strict-container.c
@@ -1,35 +1,35 @@
-#include <stdio.h>
 #include <errno.h>
-#include <sys/mman.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <linux/seccomp.h>
-#include <seccomp.h>
 #include <linux/filter.h>
+#include <linux/seccomp.h>
+#include <sched.h>
+#include <seccomp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/mount.h>
 #include <sys/ptrace.h>
 #include <sys/syscall.h>
-#include <sys/mount.h>
-#include <sched.h>
-#include <sys/syscall.h>
-#define error(arg) \
-	if (arg < 0) { \
-		perror("seccomp error"); \
-		return -1; \
-	} \
+#include <unistd.h>
+#define error(arg)                                                             \
+  if (arg < 0) {                                                               \
+    perror("seccomp error");                                                   \
+    return -1;                                                                 \
+  }
 
-int main(int argc,char *argv[]) {
-	if (argc < 2) {
-		puts("usage: ./isolate bin");
-		return 1;
-	}
-	scmp_filter_ctx context = seccomp_init(SCMP_ACT_ERRNO(EPERM));	
-	error(seccomp_rule_add(context,SCMP_ACT_ALLOW,SCMP_SYS(write),0));
-	error(seccomp_rule_add(context,SCMP_ACT_ALLOW,SCMP_SYS(read),0));
-	error(seccomp_rule_add(context, SCMP_ACT_ALLOW, SCMP_SYS(execve), 1, SCMP_A0(SCMP_CMP_EQ, (scmp_datum_t)(argv[1]))));
-	error(seccomp_rule_add(context,SCMP_ACT_ALLOW,SCMP_SYS(exit),0));
-	error(seccomp_rule_add(context,SCMP_ACT_ALLOW,SCMP_SYS(exit_group),0));
-	seccomp_load(context);
-	execl(argv[1],argv[1],NULL);
-	return 0;
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    puts("usage: ./isolate bin");
+    return 1;
+  }
+  scmp_filter_ctx context = seccomp_init(SCMP_ACT_ERRNO(EPERM));
+  error(seccomp_rule_add(context, SCMP_ACT_ALLOW, SCMP_SYS(write), 0));
+  error(seccomp_rule_add(context, SCMP_ACT_ALLOW, SCMP_SYS(read), 0));
+  error(seccomp_rule_add(context, SCMP_ACT_ALLOW, SCMP_SYS(execve), 1,
+                         SCMP_A0(SCMP_CMP_EQ, (scmp_datum_t)(argv[1]))));
+  error(seccomp_rule_add(context, SCMP_ACT_ALLOW, SCMP_SYS(exit), 0));
+  error(seccomp_rule_add(context, SCMP_ACT_ALLOW, SCMP_SYS(exit_group), 0));
+  seccomp_load(context);
+  execl(argv[1], argv[1], NULL);
+  return 0;
 }
